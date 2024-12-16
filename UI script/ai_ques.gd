748f452652831858b079f478f1ced0a76f961dd0
Biresh Biswas <workinguse5@gmail.com>
2024-12-16 13:03:49 +0530

    bug fix

--- UI script/ai_ques.gd
+++ UI script/ai_ques.gd
@@ -3,17 +3,13 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Label.text = "Category: " + str(Gobal.category)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
-func _on_label_focus_entered() -> void:
-	#pass # Replace with function body.
-	$Label.text = "Categor: " + str(Gobal.category)
 
 
 func _on_done_pressed() -> void:
